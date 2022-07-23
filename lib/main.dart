10b8a177ed9b84b9abb900a18ef534d514df0bf9
tuananh3561 <72615432+tuananh3561@users.noreply.github.com>
2022-07-23 09:29:21 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 // Import the firebase_core and cloud_firestore plugin
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_learning_firebase/page/home_page.dart';
-import 'package:flutter_learning_firebase/page/user_page.dart';
 
 bool shouldUseFirestoreEmulator = false;
 
@@ -28,11 +26,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (context) =>const HomePage(title: "home"),
-        '/user': (context) => const UserPage(),
-    },
     );
   }
 }
